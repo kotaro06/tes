59e8f65fbdf8e10cd9162779cf4bf16380c0c749
kotaro06 <kotaro.minami0686@gmail.com>
2023-05-04 06:49:53 +0700

    nomar program tanpa database

---
+++ kuliah/pertemuan10/latian1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasis </h3>
+  <table>
+    <tr>
+      <th>nomor</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <th>1</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>kotaro</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+  </table>
+</body>
+
+</html>
